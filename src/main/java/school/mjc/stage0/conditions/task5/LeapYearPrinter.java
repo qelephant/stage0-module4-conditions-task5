b70838aae566e6178f9feaa938d83b0ce7bdcc0f
qyerke <70740293+qelephant@users.noreply.github.com>
2023-02-23 10:11:25 +0600

    Update LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+       int sum = year % 400 == 0 || year %4 == 0 && year % 100 != 0 ? 1 : 0;
+        switch (sum) {
+            default:
+                System.out.println("out of range");
+                break;
+            case 0:
+                System.out.println("not leap");
+                break;
+            case 1:
+                System.out.println("leap");
+                break;
+        } 
     }
 }
